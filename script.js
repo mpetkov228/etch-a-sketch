724d58c755a9c8072ffe61a6c62dfac2a2ae6946
mpetkov228 <mpetkov228@gmail.com>
2023-04-30 17:26:14 +0300

    Add cell coloring on mouseover

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const container = document.querySelector('.container');
 container.style.width = `${16 * 27}px`;
 
+container.addEventListener('mouseover', e => {
+    if (e.target.className == 'container') return;
+
+    e.target.classList.add('filled');
+});
+
 function createPad() {
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
